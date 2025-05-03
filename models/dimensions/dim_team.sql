b9b201efa9183f3a1993dfd7b00df98c5cc5740f
julienbr-charlet <julienbr.charlet@gmail.com>
2025-05-03 10:41:02 +0200

    Update dim_team.sql

--- models/dimensions/dim_team.sql
+++ models/dimensions/dim_team.sql
@@ -1,1 +1,7 @@
-
+SELECT * 
+FROM (VALUES
+    (5217337, 'Héloïse', 'Support'),
+    (5391224, 'Justine', 'Support'),
+    (5440474, 'Patrick', 'Support'),
+    (5300290, 'Raphael', 'Support')
+) AS agents (assignee_id, assignee_name, assignee_team)
