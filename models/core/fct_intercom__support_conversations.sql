8b35ff03e4f9c7ffe4636b6a0f61c98f7b2be4c4
julienbr-charlet <julienbr.charlet@gmail.com>
2025-05-03 10:43:22 +0200

    Update fct_intercom__support_conversations.sql

--- models/core/fct_intercom__support_conversations.sql
+++ models/core/fct_intercom__support_conversations.sql
@@ -1,1 +1,24 @@
+WITH stg_intercom__conversations AS (
+  SELECT * FROM {{ ref('stg_intercom__conversations') }}
+),
 
+int_intercom__messages AS (
+  SELECT * FROM {{ ref('int_intercom__messages') }}
+),
+
+dim_team AS (
+  SELECT * FROM {{ ref('dim_team') }}
+)
+
+SELECT
+
+c.*,
+m.*,
+t.assignee_name
+
+FROM stg_intercom__conversations c
+
+LEFT JOIN int_intercom__messages m USING (conversation_id)
+LEFT JOIN dim_team t ON c.conversation_assignee_id = t.assignee_id
+
+WHERE t.assignee_team = 'Support'
