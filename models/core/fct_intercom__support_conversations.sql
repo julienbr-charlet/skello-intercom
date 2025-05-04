5d542d5f92296d38f7b9ee3b1897f1c85f8694a6
julienbr-charlet <julienbr.charlet@gmail.com>
2025-05-04 09:48:03 +0200

    Update fct_intercom__support_conversations.sql

--- models/core/fct_intercom__support_conversations.sql
+++ models/core/fct_intercom__support_conversations.sql
@@ -13,12 +13,13 @@ dim_team AS (
 SELECT
 
   c.*,
+  CASE WHEN c.conversation_state = 'closed' THEN datediff('minute',c.conversation_created_at,m.last_answer_created_at) END as conversation_time_to_close,
   t.assignee_name AS conversion_assignee_name,
   m.*
 
 FROM stg_intercom__conversations c
 
-LEFT JOIN int_intercom__messages m USING (conversation_id)
+INNER JOIN int_intercom__messages m USING (conversation_id)
 LEFT JOIN dim_team t ON c.conversation_assignee_id = t.assignee_id
 
 WHERE t.assignee_team = 'Support'
